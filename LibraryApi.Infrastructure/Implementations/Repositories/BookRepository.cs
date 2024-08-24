cde2417cab39db5c1b0c3b08346d93dbd0a063ec
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-08-24 16:35:23 +0300

    Deleted error handling in the Repositories implementantions

--- LibraryApi.Infrastructure/Implementations/Repositories/BookRepository.cs
+++ LibraryApi.Infrastructure/Implementations/Repositories/BookRepository.cs
@@ -3,6 +3,7 @@ using LibraryApi.Domain.Models;
 using LibraryApi.Infrastructure.Implementations.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace LibraryApi.Infrastructure.Implementations.Repositories
 {
@@ -13,122 +14,95 @@ namespace LibraryApi.Infrastructure.Implementations.Repositories
 
         public override async Task<IEnumerable<Book>> All()
         {
-            try
-            {
-                return await _dbSet.Where(x => x.Status == 1)
-                    .AsNoTracking()
-                    .AsSplitQuery()
-                    .OrderBy(x => x.Title)
-                    .ToListAsync();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "{Repo} All function error", typeof(BookRepository));
-                throw;
-            }
+
+            return await _dbSet.Where(x => x.Status == 1)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .OrderBy(x => x.Title)
+                .ToListAsync();
+
         }
 
         public override async Task<bool> Delete(int id)
         {
-            try
-            {
-                var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
 
-                if (result == null)
-                    return false;
+            var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
 
-                result.Status = 0;
-                result.UpdatedAt = DateTime.UtcNow;
+            if (result == null)
+                return false;
 
-                return true;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "{Repo} Delete function error", typeof(BookRepository));
-                throw;
-            }
+            result.Status = 0;
+            result.UpdatedAt = DateTime.UtcNow;
+
+            return true;
+
         }
 
         public override async Task<bool> Update(Book book)
         {
-            try
-            {
-                var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == book.Id);
 
-                if (result == null)
-                    return false;
+            var result = await _dbSet.FirstOrDefaultAsync(x => x.Id == book.Id);
 
-                result.UpdatedAt = DateTime.UtcNow;
-                result.ISBN = book.ISBN;
-                result.Title = book.Title;
-                result.Genre = book.Genre;
-                result.Description = book.Description;
-                result.AuthorId = book.AuthorId;
-                result.TakenBy = book.TakenBy;
-                result.TakenAt = book.TakenAt;
-                result.ShouldBeReturnedAt = book.ShouldBeReturnedAt;
+            if (result == null)
+                return false;
+
+            result.UpdatedAt = DateTime.UtcNow;
+            result.ISBN = book.ISBN;
+            result.Title = book.Title;
+            result.Genre = book.Genre;
+            result.Description = book.Description;
+            result.AuthorId = book.AuthorId;
+            result.TakenBy = book.TakenBy;
+            result.TakenAt = book.TakenAt;
+            result.ShouldBeReturnedAt = book.ShouldBeReturnedAt;
 
 
-                return true;
-            }
-            catch (Exception e)
-            {
-
-                _logger.LogError(e, "{Repo} Update function error", typeof(BookRepository));
-                throw;
-            }
+            return true;
         }
+
+
 
 
         public virtual async Task<IEnumerable<Book>> GetAllBooksByAuthor(int id)
         {
-            try
-            {
-                return await _dbSet.Where(x => x.AuthorId == id && x.Status == 1)
-                    .AsNoTracking()
-                    .AsSplitQuery()
-                    .OrderBy(x => x.Title)
-                    .ToListAsync();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "{Repo} GetAllBooksByAuthor function error", typeof(BookRepository));
-                throw;
-            }
+
+            return await _dbSet.Where(x => x.AuthorId == id && x.Status == 1)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .OrderBy(x => x.Title)
+                .ToListAsync();
         }
+
         public async Task<IEnumerable<Book>> GetAllBooksByUserLogin(int id)
         {
-            try
-            {
-                return await _dbSet.Where(x => x.TakenBy == id && x.Status == 1)
-                    .AsNoTracking()
-                    .AsSplitQuery()
-                    .OrderBy(x => x.Title)
-                    .ToListAsync();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "{Repo} GetAllBooksByUserLogin function error", typeof(BookRepository));
-                throw;
-            }
+
+            return await _dbSet.Where(x => x.TakenBy == id && x.Status == 1)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .OrderBy(x => x.Title)
+                .ToListAsync();
         }
+
+
         public async Task<Book?> GetByISBN(int isbn)
         {
-            try
-            {
-                return await _dbSet.FirstOrDefaultAsync(x => x.ISBN == isbn && x.Status == 1);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "{Repo} GetByISBN function error", typeof(BookRepository));
-                throw;
-            }
+
+            return await _dbSet.FirstOrDefaultAsync(x => x.ISBN == isbn && x.Status == 1);
+
         }
-        public Task<bool> GiveBookToUser(int bookId, int UserId)
+        public async Task<bool> GiveBookToUser(int bookId, int UserId)
         {
-            throw new NotImplementedException();
+            var book = _dbSet.FirstAsync(x => x.Id == bookId);
+
+            book.Result.TakenBy = UserId;
+            book.Result.TakenAt = DateOnly.FromDateTime(DateTime.Today);
+            book.Result.ShouldBeReturnedAt = DateOnly.FromDateTime(DateTime.Today.AddMonths(1));
+
+            var res = await Update(book.Result);
+            if (res)
+                return true;
+            else return false;
+
         }
-
-
     }
 }
